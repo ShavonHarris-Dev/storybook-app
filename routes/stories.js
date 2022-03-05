4909eb96c45372e0b5c9b5e8b15c1efe72a95beb
Shavon Harris <Shavonharris9114@gmail.com>
2022-03-05 10:58:10 +0000

    added delete functionality

--- routes/stories.js
+++ routes/stories.js
@@ -43,25 +43,54 @@ router.get('/', ensureAuth, async (req, res) => {
     }
   })
 
+
+  // @desc Show single story
+//@route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id)
+    .populate('user')
+    .lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    res.render('stories/show', {
+      story
+    })
+
+  } catch (err){
+    console.error(err)
+    return res.render('error/404')
+  }
+})
+
 // @desc Show edit page
 //@route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  const story = await Story.findOne({
-    _id: req.params.id
-  }).lean()
-
-  if (!story) {
-    return res.render('error/404')
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id
+    }).lean()
+  
+    if (!story) {
+      return res.render('error/404')
+    }
+  
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else{
+      res.render('stories/edit', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
   }
-
-  if (story.user != req.user.id) {
-    res.redirect('/stories')
-  } else{
-    res.render('stories/edit', {
-      story,
-    })
-  }
-})
+  })
+ 
 // @desc    Update story
 // @route   PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
@@ -88,6 +117,37 @@ router.put('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc Delete Story
+//@route GET /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.removeOne({ _id: req.params.id }) 
+    res.redirect('/dashboard')
+  } catch (err) {
+ return res.render('error/500')
+  }
+})
 
-// .lean if youre pasing it into your template 
+// @desc User Stories
+//@route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async(req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
+    .populate('user')
+    .lean()
+
+    res.render('stories/index', {
+      stories
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+
+// .lean if youre parsing it into your template 
 module.exports = router
